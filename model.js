bd5b6d9bd9749e6ab01908161de7e3c0b5e37759
a4358 <a4358@mail.ru>
2022-10-21 17:46:31 +0300

    Add files via upload

---
+++ model.js
@@ -0,0 +1,107 @@
+export const NORTH = 0;
+export const WEST = 1;
+export const SOUTH = 2;
+export const EAST = 3;
+// coordinate system: 
+// 0x ->
+// y
+// |
+// V
+export class Board {
+    constructor(x,y){
+        this.x = x;
+        this.y = y;
+        this.apples = [];
+    }
+
+    flatcoords(_x,_y){
+        return (this.x*_y+_x);
+    }
+
+    xycoords(flat){
+        let ry = 0
+        while (flat >= this.x) {
+            flat -= this.x;
+            ry += 1;
+        }
+        return [flat, ry];
+    }
+
+    checkwallcollision(_x,_y){
+        if (_x < 0 || _x>=this.x || _y < 0 || _y >= this.y) return true;
+        else return false;
+    }
+    
+    addapple(appleid){
+        //let appleid = this.flatcoords((_x,_y));
+        if (!this.apples.includes(appleid)) this.apples.push(appleid);
+    }
+
+    removeapple(appleid){
+        if (this.apples.includes(appleid)){
+            const removalindex = this.apples.indexOf(appleid);
+            this.apples.splice(removalindex,1);
+        }
+    }
+
+}
+export class Snake{
+    constructor(board, startinglength = 3,startingx = 0,startingy = 0){
+        this.dead = false;
+        this.board = board;
+        this.length = startinglength;
+        this.body = [];
+        for (let i = 0; i < startinglength; i++){
+            this.body[i]=this.board.flatcoords(startingx,startingy);
+        }
+    }
+    die(){
+        this.dead = true;
+    }
+    checkselfcollision(){
+        //console.log("checking self-collision");
+
+        if (this.body.slice(1).includes(this.body[0])) {
+            this.die();
+            console.log("snake ran into itself!");
+    }
+    }
+    move(direction){
+        console.log(`moving ${direction}`);
+        let x, y;
+        [x,y] = this.board.xycoords(this.body[0]);
+        //console.log(this.body[0]);
+        switch (direction) {
+            case NORTH:
+                if (!this.board.checkwallcollision(x, y - 1)) this.body.unshift(this.board.flatcoords(x,y-1));
+                else this.die();
+                break;
+            case SOUTH:
+                if (!this.board.checkwallcollision(x, y + 1)) this.body.unshift(this.board.flatcoords(x,y+1));
+                else this.die();
+                break;
+            case WEST:
+                if (!this.board.checkwallcollision(x - 1, y)) this.body.unshift(this.board.flatcoords(x-1,y));
+                else this.die();
+                break;
+            case EAST:
+                if (!this.board.checkwallcollision(x + 1, y)) this.body.unshift(this.board.flatcoords(x+1,y));
+                else this.die();
+                break;
+        }
+        //console.log(this.body[0]);
+
+        this.checkselfcollision();
+        this.checkapple();
+        if (this.body.length > this.length) this.body.pop();
+    }
+
+    checkapple(){
+        const appleid = this.body[0];
+        if (this.board.apples.includes(appleid)){
+            this.length += 1;
+            this.board.removeapple(appleid);
+        }
+    }
+
+}
---
+++ view.js
@@ -0,0 +1,61 @@
+export default class View{
+    constructor(canvas,cellsize = 20){
+        this.canvas = canvas;
+        this.ctx = canvas.getContext('2d');
+        this.cellsize = cellsize;
+        this.drawnsegments = 0;
+        this.board = null;
+    }
+
+    clear_view(){
+        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height)
+    }
+
+    initialise(snake){
+        console.log("initialised");
+        this.canvas.width = (snake.board.x) * this.cellsize;
+        this.canvas.height = (snake.board.y) * this.cellsize;
+        this.board = snake.board;
+        this.drawsegment(snake.body[0]);
+        this.drawnsegments = 1;
+    }
+
+    update(snake){
+        console.log(`drawing head ${snake.body[0]}`);
+        this.drawsegment(snake.body[0]);
+        console.log(snake.length, this.drawnsegments);
+        if (snake.length === this.drawnsegments+1){
+            console.log(`removing tail ${snake.body[snake.length-1]}`);
+            this.drawnsegments -=1;
+
+            this.removesegment(snake.body[snake.length-1]);
+        }
+        for (let appleid of snake.board.apples){
+            console.log("drawing apple");
+
+            this.drawapple(appleid);
+        }
+
+        this.drawnsegments += 1;
+    }
+
+    drawsegment(segmentid){
+        let x, y;
+        [x,y] = this.board.xycoords(segmentid);
+        this.ctx.fillStyle = "blue";
+        this.ctx.fillRect(x*this.cellsize,y*this.cellsize,this.cellsize,this.cellsize);
+    }
+
+    drawapple(segmentid){
+        let x, y;
+        [x,y] = this.board.xycoords(segmentid);
+        this.ctx.fillStyle = "green";
+        this.ctx.fillRect(x*this.cellsize,y*this.cellsize,this.cellsize,this.cellsize);
+    }
+
+    removesegment(segmentid){
+        let x, y;
+        [x,y] = this.board.xycoords(segmentid);
+        this.ctx.clearRect(x*this.cellsize,y*this.cellsize,this.cellsize,this.cellsize);
+    }
+}
